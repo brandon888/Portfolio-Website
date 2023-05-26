868a505a009f40bcdffffc1ed71fff8d86d6831d
brandon888 <51475015+brandon888@users.noreply.github.com>
2023-05-25 22:34:33 -0400

    finished projects

--- src/Components/ProjectText.js
+++ src/Components/ProjectText.js
@@ -15,16 +15,33 @@ const ProjectText = ({onPositionChange}) => {
       }}
       >
       <h1 className='Project-header'>{`PROJECTS`}</h1>
+      
       <div className='Project-buttons'>
-        <div>
-          <a className='button' href="https://github.com/brandon888/Portfolio-Website" target="_blank">This website</a>
+        <div className='Indiv-project'>
+          <a className='button' href="https://github.com/brandon888/Portfolio-Website" target="_blank">Website 🔗</a>
           <div className='Website-description'>
             <h1>{`\A visual introduction to myself and a showcase of\nprevious work created using ReactJS and MatterJS
             `}</h1>
           </div>
         </div>
         <div className='Indiv-project'>
-          <a className='button' href="https://github.com/brandon888/SPACBot" target="_blank">SPACBOT</a>
+          <a className='button' href="https://github.com/brandon888/SPACBot" target="_blank">SPACBOT 🔗</a>
+          <div className='Website-description'>
+            <h1>{`A bot that scrapes the web and collects data from\nReddit, Yahoo Finance, Google News, and SEC.gov\non SPAC tickers and relays information to Discord.
+            `}</h1>
+          </div>
+        </div>
+      </div>
+      <div className='Project-buttons1'>
+        <div className='Indiv-project'>
+          <a className='button' href="https://github.com/brandon888/Portfolio-Website" target="_blank">Website 🔗</a>
+          <div className='Website-description'>
+            <h1>{`\A visual introduction to myself and a showcase of\nprevious work created using ReactJS and MatterJS
+            `}</h1>
+          </div>
+        </div>
+        <div className='Indiv-project'>
+          <a className='button' href="https://github.com/brandon888/SPACBot" target="_blank">SPACBOT 🔗</a>
           <div className='Website-description'>
             <h1>{`A bot that scrapes the web and collects data from\nReddit, Yahoo Finance, Google News, and SEC.gov\non SPAC tickers and relays information to Discord.
             `}</h1>
